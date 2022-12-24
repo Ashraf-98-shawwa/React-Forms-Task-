d5384e22a6174ee3a3be673a5433dff878b7ddb1
Ashraf shawwa <ashraf_shawa@hotmail.com>
2022-12-24 15:30:34 +0200

    Sign in and sign up -  left side coding

---
+++ src/Pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import LeftSide from "../Components/LeftSide"
+export default class SignUp extends Component {
+  render() {
+    return (
+        <div>
+            <LeftSide page="sign-up" />
+      </div>
+    )
+  }
+}
